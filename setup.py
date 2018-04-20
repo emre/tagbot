06724dd5339865ae2992c432a5f7a4b970401445
Emre Yilmaz <mail@emreyilmaz.me>
2018-04-20 11:33:31 +0300

    Version bump to 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='steem_tagbot',
-    version='0.0.7',
+    version='0.0.8',
     packages=["tagbot",],
     url='http://github.com/emre/tagbot',
     license='MIT',
