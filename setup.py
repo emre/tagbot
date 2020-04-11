de5ab78674b143cd0ad7d183527bc7f7d5466c92
Emre <mail@emreyilmaz.me>
2020-04-11 21:01:19 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hive_tagbot',
-    version='0.1.0',
+    version='0.1.1',
     packages=["tagbot",],
     url='http://github.com/emre/tagbot',
     license='MIT',
@@ -14,5 +14,5 @@ setup(
             'tagbot = tagbot.bot:main',
         ],
     },
-    install_requires=["hivepy", "nltk", "requests"]
+    install_requires=["hivepy", "nltk", "requests", "dateutils"]
 )
